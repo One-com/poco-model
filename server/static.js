d9353fa29ac48fedfa5224f39ac2d11ade95da59
Peter Müller <munter@fumle.dk>
2011-05-19 17:11:09 +0200

    Improved static file server

--- server/static.js
+++ server/static.js
@@ -2,30 +2,12 @@
 
 /*global require, console*/
 
-var fs = require('fs'),
-    path = require('path'),
-    express = require('express'),
+var express = require('express'),
     app = module.exports = express.createServer();
 
 app.use(express.logger());
 
-app.get('*', function (req, res, next) {
-    var fileName = path.normalize(__dirname + '/..' + req.url.replace(/\?.*$/, ''));
-
-    path.exists(fileName, function (exists) {
-        if (!exists) {
-            res.send('File not found', 404);
-            return;
-        }
-
-        if (fs.statSync(fileName).isDirectory()) {
-            fileName += '/index.html';
-        }
-
-        res.contentType(fileName);
-        res.sendfile(fileName);
-    });
-});
+app.use(express['static'](__dirname + '/..'));
 
 if (!module.parent) {
     app.listen(3000);
